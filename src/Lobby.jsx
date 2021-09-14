ba2080f0b4412c24d9fa8b74dab2da4c288d92a7
Shad Sharma <shadanan@gmail.com>
2021-09-13 19:02:13 -0700

    Enable ability to join a game

--- src/Lobby.jsx
+++ src/Lobby.jsx
@@ -4,6 +4,7 @@ import { useAuth, useUser } from "./AuthContext";
 
 export default function Lobby() {
   const [gameCode, setGameCode] = useState();
+  const [joinGameCode, setJoinGameCode] = useState("");
   const [nickName, setNickName] = useState("");
   const auth = useAuth();
   const user = useUser();
@@ -12,10 +13,28 @@ export default function Lobby() {
     setNickName(event.target.value);
   };
 
+  const handleJoinCodeChange = (event) => {
+    setJoinGameCode(event.target.value);
+  };
+
   const createGame = async () => {
     const response = await fetch("/api/game", {
       method: "POST",
-      body: JSON.stringify({ nickName: nickName }),
+      body: JSON.stringify({ nick_name: nickName }),
+      headers: {
+        Authorization: "Bearer " + auth.access_token,
+        "Content-Type": "application/json",
+      },
+    });
+    const payload = await response.json();
+    setGameCode(payload["game_code"]);
+    console.log(payload);
+  };
+
+  const joinGame = async () => {
+    const response = await fetch("/api/join", {
+      method: "POST",
+      body: JSON.stringify({ nick_name: nickName, game_code: joinGameCode }),
       headers: {
         Authorization: "Bearer " + auth.access_token,
         "Content-Type": "application/json",
@@ -41,9 +60,15 @@ export default function Lobby() {
       <h1>Game Lobby</h1>
       {user ? <img src={user.picture} /> : null}
       <h2>Join Game</h2>
-      <TextField label="Name" />
-      <TextField label="Code" />
-      <Button variant="contained">Join Game</Button>
+      <TextField label="Name" value={nickName} onChange={handleChange} />
+      <TextField
+        label="Code"
+        value={joinGameCode}
+        onChange={handleJoinCodeChange}
+      />
+      <Button variant="contained" onClick={joinGame}>
+        Join Game
+      </Button>
       <h2>Host Game</h2>
       <TextField label="Name" value={nickName} onChange={handleChange} />
       <Button variant="contained" onClick={createGame}>
